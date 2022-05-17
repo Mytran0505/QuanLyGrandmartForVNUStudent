cd508cfd44d0416f44b97435361a00a9fa11eb63
trannguyen0505 <20520322@gm.uit.edu.vn>
2022-05-17 22:48:46 +0700

    them code cho 2 file ....

--- src/GUI/DeleteEmpForm.java
+++ src/GUI/DeleteEmpForm.java
@@ -1,6 +1,7 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+ * Tran cute qua
  */
 package GUI;
 
