07442044d839d178fa6731ae42d8913002c5d016
anhhuybee <97373758+anhhuybee@users.noreply.github.com>
2022-05-18 13:20:38 +0700

    Add files via upload

--- src/GUI/DeleteEmp2.java
+++ src/GUI/DeleteEmp2.java
@@ -1,7 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- * Tran cute
  */
 package GUI;
 
