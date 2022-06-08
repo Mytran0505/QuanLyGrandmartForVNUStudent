e81ee50d204bbef00e791489610071fcce0d0e26
quyen20520296 <105615873+quyen20520296@users.noreply.github.com>
2022-06-08 17:03:55 +0700

    insert product

--- src/GUI/InsertPro2.java
+++ src/GUI/InsertPro2.java
@@ -1,14 +1,46 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
 package GUI;
 
-/**
- *
- * @author LENOVO
- */
+import DTO.Product_DTO;
+import javax.swing.*;
+import BUS.ProductManagement_BUS;
+import java.text.SimpleDateFormat;
+
 public class InsertPro2 extends javax.swing.JFrame {
+    
+    ProductManagement_BUS busProductManagement = new ProductManagement_BUS();
+    Product_DTO newProduct = null;
+    
+    public InsertPro2(Product_DTO product){
+        initComponents();
+        newProduct = busProductManagement.getProductInfo(product);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        txtSupID.setText(String.valueOf(newProduct.getSup_id()));
+        txtProName.setText(newProduct.getName());
+        txtCountry.setText(newProduct.getCountry());
+        txtOPrice.setText(String.valueOf(newProduct.getOriginal_price()));
+        txtSPrice.setText(String.valueOf(newProduct.getSale_price()));
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        txtMFG.setText(sdf.format(newProduct.getMFG()));
+        txtEXP.setText(sdf.format(newProduct.getEXP()));
+        txtProType.setText(newProduct.getType());
+        txtVAT.setText(String.valueOf(newProduct.getVAT()));
+        txtImportedDate.setText(sdf.format(newProduct.getImported_date()));
+        txtImportedQuantity.setText(String.valueOf(newProduct.getImported_quantity()));
+        txtRemainingQuantity.setText(String.valueOf(newProduct.getRemaining_quantity()));;
+        txtSupID.disable();
+        txtProName.disable();
+        txtCountry.disable();
+        txtOPrice.disable();
+        txtSPrice.disable();
+        txtMFG.disable();
+        txtEXP.disable();
+        txtProType.disable();
+        txtVAT.disable();
+        txtImportedDate.disable();
+        txtImportedQuantity.disable();
+        txtRemainingQuantity.disable();
+    }
 
 
     @SuppressWarnings("unchecked")
@@ -19,20 +51,20 @@ public class InsertPro2 extends javax.swing.JFrame {
         jLabel5 = new javax.swing.JLabel();
         jLabel6 = new javax.swing.JLabel();
         jPanel9 = new javax.swing.JPanel();
-        txtProName2 = new javax.swing.JTextField();
+        txtProName = new javax.swing.JTextField();
         jLabel27 = new javax.swing.JLabel();
         jLabel28 = new javax.swing.JLabel();
-        txtCountry2 = new javax.swing.JTextField();
+        txtCountry = new javax.swing.JTextField();
         jLabel29 = new javax.swing.JLabel();
         txtImportedDate = new javax.swing.JTextField();
         jLabel30 = new javax.swing.JLabel();
         jLabel31 = new javax.swing.JLabel();
         jLabel32 = new javax.swing.JLabel();
         jLabel33 = new javax.swing.JLabel();
-        txtProType2 = new javax.swing.JTextField();
+        txtProType = new javax.swing.JTextField();
         jLabel34 = new javax.swing.JLabel();
         jLabel35 = new javax.swing.JLabel();
-        txtVAT2 = new javax.swing.JTextField();
+        txtVAT = new javax.swing.JTextField();
         jLabel36 = new javax.swing.JLabel();
         jLabel37 = new javax.swing.JLabel();
         txtSupID = new javax.swing.JTextField();
@@ -40,7 +72,7 @@ public class InsertPro2 extends javax.swing.JFrame {
         txtRemainingQuantity = new javax.swing.JTextField();
         txtOPrice = new javax.swing.JTextField();
         txtImportedQuantity = new javax.swing.JTextField();
-        txtSPrice3 = new javax.swing.JTextField();
+        txtSPrice = new javax.swing.JTextField();
         txtMFG = new javax.swing.JTextField();
         txtEXP = new javax.swing.JTextField();
         btnConfirm = new javax.swing.JButton();
@@ -79,122 +111,98 @@ public class InsertPro2 extends javax.swing.JFrame {
         jPanel9.setBackground(new java.awt.Color(239, 250, 252));
         jPanel9.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        txtProName2.setBackground(new java.awt.Color(255, 255, 255));
-        txtProName2.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        txtProName2.setHorizontalAlignment(javax.swing.JTextField.LEFT);
-        jPanel9.add(txtProName2, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 80, 200, 30));
+        txtProName.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        txtProName.setHorizontalAlignment(javax.swing.JTextField.LEFT);
+        jPanel9.add(txtProName, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 80, 200, 30));
 
         jLabel27.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        jLabel27.setForeground(new java.awt.Color(0, 0, 0));
         jLabel27.setText("Product name:");
         jPanel9.add(jLabel27, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 80, 127, 30));
 
         jLabel28.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        jLabel28.setForeground(new java.awt.Color(0, 0, 0));
         jLabel28.setText("Country:");
         jPanel9.add(jLabel28, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 120, 127, 30));
 
-        txtCountry2.setBackground(new java.awt.Color(255, 255, 255));
-        txtCountry2.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        txtCountry2.setHorizontalAlignment(javax.swing.JTextField.LEFT);
-        jPanel9.add(txtCountry2, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 120, 200, 30));
+        txtCountry.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        txtCountry.setHorizontalAlignment(javax.swing.JTextField.LEFT);
+        jPanel9.add(txtCountry, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 120, 200, 30));
 
         jLabel29.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        jLabel29.setForeground(new java.awt.Color(0, 0, 0));
         jLabel29.setText("Original price");
         jPanel9.add(jLabel29, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 160, 127, 30));
 
-        txtImportedDate.setBackground(new java.awt.Color(255, 255, 255));
         txtImportedDate.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         txtImportedDate.setHorizontalAlignment(javax.swing.JTextField.LEFT);
         jPanel9.add(txtImportedDate, new org.netbeans.lib.awtextra.AbsoluteConstraints(590, 120, 200, 30));
 
         jLabel30.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        jLabel30.setForeground(new java.awt.Color(0, 0, 0));
         jLabel30.setText("Sale price");
         jPanel9.add(jLabel30, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 200, 127, 30));
 
         jLabel31.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        jLabel31.setForeground(new java.awt.Color(0, 0, 0));
         jLabel31.setText("MFG:");
         jPanel9.add(jLabel31, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 240, 127, 30));
 
         jLabel32.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        jLabel32.setForeground(new java.awt.Color(0, 0, 0));
         jLabel32.setText("EXP:");
         jPanel9.add(jLabel32, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 280, 127, 30));
 
         jLabel33.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        jLabel33.setForeground(new java.awt.Color(0, 0, 0));
         jLabel33.setText("Imported Quantity:");
         jPanel9.add(jLabel33, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 160, 160, 30));
 
-        txtProType2.setBackground(new java.awt.Color(255, 255, 255));
-        txtProType2.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        txtProType2.setHorizontalAlignment(javax.swing.JTextField.LEFT);
-        jPanel9.add(txtProType2, new org.netbeans.lib.awtextra.AbsoluteConstraints(590, 40, 200, 30));
+        txtProType.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        txtProType.setHorizontalAlignment(javax.swing.JTextField.LEFT);
+        jPanel9.add(txtProType, new org.netbeans.lib.awtextra.AbsoluteConstraints(590, 40, 200, 30));
 
         jLabel34.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        jLabel34.setForeground(new java.awt.Color(0, 0, 0));
         jLabel34.setText("Product type:");
         jPanel9.add(jLabel34, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 40, 140, 30));
 
         jLabel35.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        jLabel35.setForeground(new java.awt.Color(0, 0, 0));
         jLabel35.setText("VAT:");
         jPanel9.add(jLabel35, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 80, 90, 30));
 
-        txtVAT2.setBackground(new java.awt.Color(255, 255, 255));
-        txtVAT2.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        txtVAT2.setHorizontalAlignment(javax.swing.JTextField.LEFT);
-        jPanel9.add(txtVAT2, new org.netbeans.lib.awtextra.AbsoluteConstraints(590, 80, 200, 30));
+        txtVAT.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        txtVAT.setHorizontalAlignment(javax.swing.JTextField.LEFT);
+        jPanel9.add(txtVAT, new org.netbeans.lib.awtextra.AbsoluteConstraints(590, 80, 200, 30));
 
         jLabel36.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        jLabel36.setForeground(new java.awt.Color(0, 0, 0));
         jLabel36.setText("Imported date:");
         jPanel9.add(jLabel36, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 120, 130, 30));
 
         jLabel37.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        jLabel37.setForeground(new java.awt.Color(0, 0, 0));
         jLabel37.setText("Supplier ID:");
         jPanel9.add(jLabel37, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 40, 127, 30));
 
-        txtSupID.setBackground(new java.awt.Color(255, 255, 255));
         txtSupID.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         txtSupID.setHorizontalAlignment(javax.swing.JTextField.LEFT);
         jPanel9.add(txtSupID, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 40, 200, 30));
 
         jLabel44.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        jLabel44.setForeground(new java.awt.Color(0, 0, 0));
         jLabel44.setText("Remaining quantity:");
         jPanel9.add(jLabel44, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 200, 170, 30));
 
-        txtRemainingQuantity.setBackground(new java.awt.Color(255, 255, 255));
         txtRemainingQuantity.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         txtRemainingQuantity.setHorizontalAlignment(javax.swing.JTextField.LEFT);
         jPanel9.add(txtRemainingQuantity, new org.netbeans.lib.awtextra.AbsoluteConstraints(590, 200, 200, 30));
 
-        txtOPrice.setBackground(new java.awt.Color(255, 255, 255));
         txtOPrice.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         txtOPrice.setHorizontalAlignment(javax.swing.JTextField.LEFT);
         jPanel9.add(txtOPrice, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 160, 200, 30));
 
-        txtImportedQuantity.setBackground(new java.awt.Color(255, 255, 255));
         txtImportedQuantity.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         txtImportedQuantity.setHorizontalAlignment(javax.swing.JTextField.LEFT);
         jPanel9.add(txtImportedQuantity, new org.netbeans.lib.awtextra.AbsoluteConstraints(590, 160, 200, 30));
 
-        txtSPrice3.setBackground(new java.awt.Color(255, 255, 255));
-        txtSPrice3.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        txtSPrice3.setHorizontalAlignment(javax.swing.JTextField.LEFT);
-        jPanel9.add(txtSPrice3, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 200, 200, 30));
+        txtSPrice.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        txtSPrice.setHorizontalAlignment(javax.swing.JTextField.LEFT);
+        jPanel9.add(txtSPrice, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 200, 200, 30));
 
-        txtMFG.setBackground(new java.awt.Color(255, 255, 255));
         txtMFG.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         txtMFG.setHorizontalAlignment(javax.swing.JTextField.LEFT);
         jPanel9.add(txtMFG, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 240, 200, 30));
 
-        txtEXP.setBackground(new java.awt.Color(255, 255, 255));
         txtEXP.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         txtEXP.setHorizontalAlignment(javax.swing.JTextField.LEFT);
         jPanel9.add(txtEXP, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 280, 200, 30));
@@ -227,11 +235,21 @@ public class InsertPro2 extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnConfirmActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnConfirmActionPerformed
-
+        if(busProductManagement.insert(newProduct)){
+            JOptionPane.showMessageDialog(this, "Product information is inserted successfully!", "Success", JOptionPane.INFORMATION_MESSAGE);
+            setVisible(false);
+            InsertProForm insertForm = new InsertProForm(newProduct);
+            insertForm.setVisible(true);
+        }
+        else{
+            JOptionPane.showMessageDialog(this, "Invalid data!", "Error", JOptionPane.ERROR_MESSAGE);
+        }
     }//GEN-LAST:event_btnConfirmActionPerformed
 
     private void btnDismissActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDismissActionPerformed
-
+        setVisible(false);
+        InsertProForm insertForm = new InsertProForm(newProduct);
+        insertForm.setVisible(true);
     }//GEN-LAST:event_btnDismissActionPerformed
 
     /**
@@ -258,17 +276,17 @@ public class InsertPro2 extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel6;
     private javax.swing.JPanel jPanel6;
     private javax.swing.JPanel jPanel9;
-    private javax.swing.JTextField txtCountry2;
+    private javax.swing.JTextField txtCountry;
     private javax.swing.JTextField txtEXP;
     private javax.swing.JTextField txtImportedDate;
     private javax.swing.JTextField txtImportedQuantity;
     private javax.swing.JTextField txtMFG;
     private javax.swing.JTextField txtOPrice;
-    private javax.swing.JTextField txtProName2;
-    private javax.swing.JTextField txtProType2;
+    private javax.swing.JTextField txtProName;
+    private javax.swing.JTextField txtProType;
     private javax.swing.JTextField txtRemainingQuantity;
-    private javax.swing.JTextField txtSPrice3;
+    private javax.swing.JTextField txtSPrice;
     private javax.swing.JTextField txtSupID;
-    private javax.swing.JTextField txtVAT2;
+    private javax.swing.JTextField txtVAT;
     // End of variables declaration//GEN-END:variables
 }
