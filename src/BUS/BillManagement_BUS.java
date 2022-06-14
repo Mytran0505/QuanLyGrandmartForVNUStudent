1bda3038a4ba93574930c835a9114c5f9e00d540
anhhuybee <97373758+anhhuybee@users.noreply.github.com>
2022-06-14 10:48:21 +0700

    Add files via upload

--- src/BUS/BillManagement_BUS.java
+++ src/BUS/BillManagement_BUS.java
@@ -1,12 +1,16 @@
 package BUS;
 
 import Connection.Bill_CON;
+import DTO.BillDetails_DTO;
 import DTO.Bill_DTO;
+import DTO.Product_DTO;
 import java.util.ArrayList;
+import Connection.Report.iTextPDF_CON;
 
 
 public class BillManagement_BUS {
     Bill_CON conBill = new Bill_CON();
+    iTextPDF_CON conReport = new iTextPDF_CON();
     public boolean insert(Bill_DTO dtoBill){
         return conBill.insert(dtoBill);
     }
@@ -22,4 +26,13 @@ public class BillManagement_BUS {
     public ArrayList<Bill_DTO> getBillList(){
         return conBill.getBillList();
     }
+    public Long sumUp(BillDetails_DTO dtoBillDetails, Product_DTO dtoProduct){
+        return conBill.sumUp(dtoBillDetails, dtoProduct);
+    }
+    public Long getTotalMoney(BillDetails_DTO dtoBillDetails, Product_DTO dtoProduct){
+        return conBill.getTotalMoney(dtoBillDetails, dtoProduct);
+    }
+//    public void printBill(Bill_DTO dtoBill){
+//        conReport.printBill(dtoBill);
+//    }
 }
