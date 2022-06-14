eaddfe676b23944a41dcd0fb7bfe1925f3be4cff
trannguyen0505 <20520322@gm.uit.edu.vn>
2022-06-14 21:56:13 +0700

    add folder report

---
+++ src/Connection/REPORT/NewClass.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Connection.REPORT.FONTS;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class NewClass {
+    
+}
