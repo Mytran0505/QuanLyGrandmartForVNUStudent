758a38f3a32fa2ba425917012e0791d146e3eba3
anhhuybee <97373758+anhhuybee@users.noreply.github.com>
2022-06-16 22:38:26 +0700

    Add files via upload

---
+++ src/DTO/Supplier_DTO.java
@@ -0,0 +1,22 @@
+package DTO;
+
+
+public class Supplier_DTO {
+    private int id;
+
+    public Supplier_DTO() {
+    }
+
+    public Supplier_DTO(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+}
